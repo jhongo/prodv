e552120519f264ed0fb13accafc5b218717955e3
javierjadan6 <javierjadan6@gmail.com>
2021-12-21 23:15:49 -0500

    Inicio de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'register',
     component:RegistroComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
   },
 ];
 
