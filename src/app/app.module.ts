2bb729ff2e3c14a5c38ffe467232e4a3d1812e99
javierjadan6 <javierjadan6@gmail.com>
2021-12-28 17:01:46 -0500

    Primer deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { CampeonatosComponent } from './pages/campeonatos/campeonatos.component'
 import { InternacionalComponent } from './pages/internacional/internacional.component';
 import { NacionalComponent } from './pages/nacional/nacional.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -46,6 +47,12 @@ import { NoticiasComponent } from './pages/noticias/noticias.component';
     AngularFirestoreModule,
     AngularFireAuthModule,
     FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
 
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
