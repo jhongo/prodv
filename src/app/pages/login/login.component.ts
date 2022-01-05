0f90af5f51320005e0ee2363d46a91b8b7cd807a
javierjadan6 <javierjadan6@gmail.com>
2022-01-04 22:50:56 -0500

    validaciones de login y register

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -13,15 +13,15 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  
-  usuarios: User={
+
+  usuarios: User = {
     uid: '',
     email: '',
-    displayName:'',
+    displayName: '',
     emailVerified: null,
   };
-  uid= '';
-  suscriberUserInfo : Subscription;
+  uid = '';
+  suscriberUserInfo: Subscription;
 
   constructor(public firebaseauthService: FirebaseauthService,
     public fireStore: FirestoreService,
@@ -29,29 +29,59 @@ export class LoginComponent implements OnInit {
     public toastController: ToastController,
     public alertController: AlertController,
     private menuL: MenuController,
-    public router: Router, ) { }
+    public router: Router,) { }
 
   ngOnInit() {
 
     this.menuL.enable(false);
   }
 
-  async ingresar(email,password){
-   
-    await this.firebaseauthService.login(email.value, password.value).then( res=> {
+  async ingresar(email, password) {
+
+    await this.firebaseauthService.login(email.value, password.value).then(res => {
       console.log('Ingreso con exito');
-      // this.menuL.enable(true);
-      this.router.navigate(['/tab-inicial/partidos']);
-      this.usuarios={
+      this.presentLoading('Iniciando Sesión', 1500);
+      setTimeout(() => {
+        this.router.navigate(['/tab-inicial/partidos']);
+        this.menuL.enable(true);
+      }, 1000); 
+      this.usuarios = {
         uid: null,
         email: null,
-        displayName:null,
-        emailVerified:null,
+        displayName: null,
+        emailVerified: null,
       };
-      this.menuL.enable(true);
-    }).catch( res=> {
-      console.log('erros => ', res.message)
+    }).catch(res => {
+      console.log('error => ', res.message)
+      this.presentAlert("Verifique sus datos");
     })
+  }
+
+  async presentToast(mensaje: string, tiempo: number) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: tiempo
+    });
+    toast.present();
+  }
+
+  async presentLoading(mensaje: string, tiempo: number) {
+    const loading = await this.loadingController.create({
+      message: mensaje,
+      duration: tiempo
+    });
+    await loading.present();
+  }
+
+  async presentAlert(mensaje: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Error',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
 }
