124a327bbbd0e2912a6e59e02b3d2232e2298163
jhongo <jhon_gom22@outlook.com>
2022-01-20 16:51:52 -0500

    Pruebas main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  modes =[ 'moth 1','moth 2','moth 3','moth 4','moth 5','moth 6','moth 7','moth 8','moth 9', ];
+  selectedMode = 'moth 1';
+
+  heroes = ['SuperMan', 'Batman', 'Linterna Verde','Wonder Woman']
+
   constructor() { }
 
   ngOnInit() {}
