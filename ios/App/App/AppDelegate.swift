22b265d8e20f3f198905b7bd6110fb0f4476c36c
john <home@homes-MacBook-Pro.local>
2022-04-26 16:24:49 -0500

    Entorno para IOS

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,15 +1,13 @@
 import UIKit
 import Capacitor
-import Firebase
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate{
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
         return true
     }
 
