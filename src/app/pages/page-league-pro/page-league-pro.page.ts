032ed9711aea7a5241a66c94d916afd69a135e1b
jhongo <jhon_gom22@outlook.com>
2022-09-16 15:28:33 -0500

    Cambio en peticiones

--- src/app/pages/page-league-pro/page-league-pro.page.ts
+++ src/app/pages/page-league-pro/page-league-pro.page.ts
@@ -18,34 +18,34 @@ export class PageLeagueProPage implements OnInit {
 
   ngOnInit() {
 
-    // this.http.get<any>('https://api.b365api.com/v1/league',{
-    //   params:{
-    //     'sport_id' : '1',
-    //     'cc' : 'ec', 
-    //     'token': '135086-6tOga3UOkyFKVZ'
-    //   }
-    // }).subscribe( res =>{
-    //   // console.log(res);
-    //   // this.campeonatosEcuatorianos = res.results;
-    //   // console.log(this.campeonatosEcuatorianos);
+     this.http.get<any>('https://api.b365api.com/v1/league',{
+       params:{
+         'sport_id' : '1',
+         'cc' : 'ec', 
+         'token': '135086-6tOga3UOkyFKVZ'
+       }
+     }).subscribe( res =>{
+     console.log(res);
+     this.campeonatosEcuatorianos = res.results;
+     console.log(this.campeonatosEcuatorianos);
 
-    // });
+     });
 
 
 
     // Get Table Positions
 
-    this.http.get<any>('https://api.b365api.com/v2/league/table',{
-      params:{
-        'league_id' : '540',
-        'token': '135086-6tOga3UOkyFKVZ'
-      }
-    }).subscribe( res =>{
-      console.log(res);
-      this.positionsGroup = res.results;
-      console.log(this.positionsGroup);
+    // this.http.get<any>('https://api.b365api.com/v2/league/table',{
+    //   params:{
+    //     'league_id' : '540',
+    //     'token': '135086-6tOga3UOkyFKVZ'
+    //   }
+    // }).subscribe( res =>{
+    //   console.log(res);
+    //   this.positionsGroup = res.results;
+    //   console.log(this.positionsGroup);
 
-    });
+    // });
 
   }
 
